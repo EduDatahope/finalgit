72cc31ffcf189b7298bd5a132702ecd6d8a28874
egonzo <162932324+EduDatahope@users.noreply.github.com>
2024-09-01 20:21:25 -0500

    v2.0.0 ejercicio estable

---
+++ func_/my_util_.py
@@ -0,0 +1,7 @@
+def hola1():
+    print('hola ... lograste importar de un folder :3')
+
+if __name__=='__main__':
+    hola1()
+else:
+    print('importando func_/my_util')
--- main_.py
+++ main_.py
@@ -1,9 +1,11 @@
 
 import functionality_
+import func_.my_util_
 
 if __name__ == '__main__':
    print('-------inicio------')
    print('linea 1')
    functionality_.func_x()
    functionality_.func_y()
+   func_.my_util_.hola1()
    print('------fin-------')
