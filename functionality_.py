257a368893f4bfa4f83ca50f5bf70ab4116bdb76
egonzo <162932324+EduDatahope@users.noreply.github.com>
2024-09-01 19:59:36 -0500

    v1.5 funcionando

---
+++ functionality_.py
@@ -0,0 +1,8 @@
+
+def func_x():
+    print('funcion x')
+
+if __name__ =='__main__':
+    print('modulo funciones')
+else:
+   print('modulo importado')
--- main_.py
+++ main_.py
@@ -1,7 +1,8 @@
 
-
+import functionality_
 
 if __name__ == '__main__':
+   print('-------inicio------')
    print('linea 1')
-
-
+   functionality_.func_x()
+   print('------fin-------')
