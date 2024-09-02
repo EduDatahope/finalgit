1e4d26ea62812395ffab5c16e473e32275bb62fd
egonzo <162932324+EduDatahope@users.noreply.github.com>
2024-09-01 20:08:27 -0500

    funciones 2.0

--- functionality_.py
+++ functionality_.py
@@ -1,6 +1,9 @@
 
 def func_x():
-    print('funcion x')
+    print('funcion x ... :v')
+
+def func_y():
+    print('funcion y ... :D')
 
 if __name__ =='__main__':
     print('modulo funciones')
--- main_.py
+++ main_.py
@@ -5,4 +5,5 @@ if __name__ == '__main__':
    print('-------inicio------')
    print('linea 1')
    functionality_.func_x()
+   functionality_.func_y()
    print('------fin-------')
